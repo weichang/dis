a308d250b924c9e5e0823d8e522234cd95a1a5d7
Jeffrey <Jeffrey@cloud-interactive.com>
2016-07-26 15:43:08 +0800

    使用ModelFactory 產生數據

---
+++ app/Discussion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Discussion extends Model
+{
+    //
+    protected $fillable = ['title','body','user_id','last_user_id'];
+}
